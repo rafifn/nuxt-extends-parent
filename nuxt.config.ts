7d65341722f973ae75923224c395c8208040cf25
rafifn <rafiiferrynantyo@gmail.com>
2023-03-30 12:13:07 +0700

    autoimport stores

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   imports: {
-    dirs: ['composables', 'composables/**/index.ts'],
+    dirs: ['composables', 'composables/**/index.ts', 'stores'],
   }
 })
