b02bb081142ce70b8a490f700cf190aeeb9c40fe
rafifn <rafiiferrynantyo@gmail.com>
2023-03-30 08:54:01 +0700

    first init parent

---
+++ components/atoms/AButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <button>ini AButton Component</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/molecules/MInfo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div class="alert">ini MInfo Component</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
